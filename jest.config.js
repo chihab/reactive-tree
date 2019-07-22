174c1852411c9df2dd1b90094f3a24169794def1
Chihab Otmani <chihab@gmail.com>
2019-07-22 16:18:12 +0100

    chore: init ts monorepo

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/packages/core"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+}
---
+++ packages/core/__tests__/core.test.ts
@@ -0,0 +1,4 @@
+"use strict";
+test("the app is for realz", () => {
+  expect("realsies").toEqual("realsies");
+});
