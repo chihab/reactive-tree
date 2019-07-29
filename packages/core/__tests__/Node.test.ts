d7bd2341afe9737653728ebbb484dc536ca3ba78
Chihab Otmani <chihab@gmail.com>
2019-07-29 08:27:57 +0100

    test: add node tests

--- packages/core/__tests__/Node.test.ts
+++ packages/core/__tests__/Node.test.ts
@@ -1,10 +1,10 @@
-import { Node, Leaf } from "../src";
+import { Leaf, Node } from "../src";
 
 const add = (children: number[]): number => {
   return children.reduce((sum, val) => sum + val, 0);
 };
 
-test("create node with empty constructor", done => {
+test("subscribe to a node having leaves as children", done => {
   const leaf1: Leaf<number> = new Leaf(6);
   const leaf2: Leaf<number> = new Leaf(11);
   const node: Node<number> = new Node([leaf1, leaf2], add);
@@ -14,13 +14,14 @@ test("create node with empty constructor", done => {
   });
 });
 
-test("create node with empty constructor", done => {
+test("subscribe to a node having nodes as children", done => {
   const leaf1: Leaf<number> = new Leaf(6);
   const leaf2: Leaf<number> = new Leaf(11);
-  const node: Node<number> = new Node([leaf1, leaf2], add);
-  const root: Node<number> = new Node([node], ([v]) => v);
+  const node1: Node<number> = new Node([leaf1, leaf2], add);
+  const node2: Node<number> = new Node([leaf1, leaf1], add);
+  const root: Node<number> = new Node([node1, node2], ([v1, v2]) => v1 - v2);
   root.output$.subscribe(v => {
-    expect(v).toBe(17);
+    expect(v).toBe(5);
     done();
   });
 });
